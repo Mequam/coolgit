bd0b114fd5918ad33e875094d27eaa0c6be41ceb
Mequam <blue9ja@gmail.com>
2021-06-05 08:06:19 -0400

    added library path initilization

--- gladosRepo/init_scripts/coolgit-init.sh
+++ gladosRepo/init_scripts/coolgit-init.sh
@@ -8,6 +8,14 @@ mkdir $REPO_PATH || exit -1
 cd $REPO_PATH || exit -1
 git init --bare
 
+#make sure we have the shebang if it is not already present
+test -f hooks/post-receive || echo \#!/bin/bash > hooks/post-receive 
+#create the first lines of the post-recive file to tell it where to find the color_effects file
+
+echo \$COLOR_EFFECTS=$START_PATH/color_effects >> hooks/post-receive
+echo \$SOUND_EFFECTS=$2 >> hooks/post-receive
+echo \$SOUND_LIBRARY=$START_PATH/sound_manip.sh >> hooks/post-receive
+
 #add our hooks to the post-receive queue
 cat $START_PATH/post-receive >> hooks/post-receive
 
